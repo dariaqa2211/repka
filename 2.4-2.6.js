ac8e8a7eff6117dc7a10d35cd67056bd9ded28bf
Dasha Gerbutova <dashagerbutova@MacBook-Air-Dasha.local>
2024-05-02 13:42:18 +0300

    Added 2.4-2.6

---
+++ 2.4-2.6.js
@@ -0,0 +1,48 @@
+//2.4 Переменные
+
+//Работа с переменными
+/*
+let admin;
+let name;
+name='John';
+admin=name;
+alert(admin);
+*/
+
+//Придумайте правильные имена
+/*
+const OUR_PLANET_NAME;
+let currentUserName;
+*/
+
+//Какие буквы (заглавные или строчные) использовать для имён констант?
+/*
+const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы
+const age = someCode(BIRTHDAY); // а здесь нет
+*/
+
+
+
+//2.5 Типы данных
+
+//Шаблонные строки
+/*
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
+*/
+
+
+
+//2.6 Взаимодействие: alert, prompt, confirm
+
+//Простая страница
+/*
+let userName;
+userName = prompt("Введите свое имя", '');
+alert(`Ого, ты - ${userName}`);
+*/
